b2f7f61143a0d9f82b6d7a10f6cc70ed3b3fde07
jobcrackerbah <erik@critiqueit.com>
2016-05-25 19:06:57 -0700

    renames socket server for clarity

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,12 +1,14 @@
-
-module.exports = function(app, express, io){
+var path = require('path')
+module.exports = function(app, express, socketedServer){
     // Static routes
     app.use(express.static(__dirname + '/../client/'));
-
+    app.get('/', function(request, response){
+        response.sendFile(path.join(__dirname, "/../controllers/test/test.html"));
+    })
     // Module routes
     require('./authentication.routes.js')(app, express)
     require('./client.routes.js')(app, express)
-    require('./fileService.routes.js')(app, express, io)
+    require('./fileService.routes.js')(app, express, socketedServer)
 }
 
 
