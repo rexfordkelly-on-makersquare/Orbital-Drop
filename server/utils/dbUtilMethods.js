65a42bae06f4314cb6368ca9c646e68cdc8870ff
yaoandrew <yaoandrew@yahoo.com>
2016-05-27 14:00:54 -0700

    validtion library added to prevent dupes

--- server/utils/dbUtilMethods.js
+++ server/utils/dbUtilMethods.js
@@ -13,66 +13,65 @@ var helpers = {
 
     user.save(function(err) {
       if (err) {
-        console.log("New user not added to DB");
-        throw err;
-      }
-
-      console.log("New user successfully added to DB");
+        console.log (err);
+      } else {
+          console.log("New user successfully added to DB");
+        }
     });
   },
 
   addToBlackListDbase : function (username, blockedUser) {
 
-  UsersModel.update({username: username}, {$push:{blackList : blockedUser}}, 
+  UsersModel.update({username: username}, {$push:{blackList : blockedUser}},
     function (err){
       if (err) {
         console.log("Add to blacklist failed");
-        throw err;
-      }
-
-    console.log("Blacklist updated in DB")
-    })
+        console.log (err);
+      } else {
+          console.log("Blacklist updated in DB");
+        }
+    });
 },
 
     addToWhiteListDbase : function (username, approvedUser) {
 
-  UsersModel.update({username: username}, {$push:{whiteList : approvedUser}}, 
+  UsersModel.update({username: username}, {$push:{whiteList : approvedUser}},
     function (err){
       if (err) {
         console.log("Add to whitelist failed");
-        throw err;
-      }
-
-    console.log("Whitelist updated in DB")
-    })
+        console.log (err);
+      } else {
+          console.log("Whitelist updated in DB");
+        }
+    });
 },
 
   removeFromBlackListDbase : function (username, blockedUser) {
 
-  UsersModel.update({username: username}, {$pull:{blackList : blockedUser}}, 
+  UsersModel.update({username: username}, {$pull:{blackList : blockedUser}},
     function (err){
       if (err) {
         console.log("Remove from blacklist failed");
-        throw err;
-      }
-
-    console.log("Blacklist user removed in DB")
-    })
+        console.log (err);
+      } else {
+          console.log("Blacklist user removed in DB");
+        }
+    });
 },
 
   removeFromWhiteListDbase : function (username, approvedUser) {
 
-  UsersModel.update({username: username}, {$pull:{whiteList : approvedUser}}, 
+  UsersModel.update({username: username}, {$pull:{whiteList : approvedUser}},
     function (err){
       if (err) {
         console.log("Remove from whitelist failed");
-        throw err;
-      }
-
-    console.log("Whitelist user removed in DB")
-    })
+        console.log (err);
+      } else {
+          console.log("Whitelist user removed in DB");
+        }
+    });
   }
-}
+};
 
-module.exports = helpers
+module.exports = helpers;
 
