dab554b014f64cf2758ce71e5e03564f3c6b1a70
jobcrackerbah <erik@critiqueit.com>
2016-05-27 09:31:14 -0700

    clarifies comments and adds session check

--- server/controllers/fileServiceController.js
+++ server/controllers/fileServiceController.js
@@ -3,71 +3,80 @@ const busboy = require('busboy');
 const uuid = require('uuid');
 const path = require('path');
 const socketIO = require('socket.io');
-var users = require('../models/activeUserModel.js');
-var util = require('../utils/fileServiceUtil.js');
+const users = require('../models/activeUserModel.js');
+const util = require('../utils/fileServiceUtil.js');
 
 
 module.exports = function(express, socketedServer){
 	// adds event listeners to the http.Server instance
-	var io = socketIO(socketedServer);
+	const io = socketIO(socketedServer);
 	var userSockets = {}
-
 	/*** 
-		The users object will hold the state of our application.  When a new user establishes/ends a 
-		socket connection, we will add/remove the user instance and emit the users list to all clients.
+		The users object will hold the state of our application.  When a new user emits/ends a 
+		socket event, we will add/remove the user instance and emit the users list to all clients.
 	***/
 	io.on('connection', function(socket){
 		socket.on('createUser', function(sessionId){
 			users[sessionId] = util.createUser(socket.id, sessionId);
-			userSockets[socket.id] = socket;
+			userSockets[sessionId] = socket;
 			io.sockets.emit('updateUsers', users);
 		})
 
 		socket.on('disconnect', function () {
 			var user = util.findUserBySocketId(socket.id, users);
-
-			delete userSockets[socket.id];
+			// remove both socket and user on socket closing
+			delete userSockets[user.userId];
 		 	delete users[user.userId];
+		 	/*** ADD DELETION OF FILES IN UPLOADS FOLDER IF USER DISCONNECTS ***/
 			io.sockets.emit('updateUsers', users);
 		});
 	})
 
 	return({
 		upload : function(request, response, error){
+			// add middleware validation
+			var userId;
+			if(request.session.passport){
+				userId = request.session.passport.user
+			}else{
+				res.status(500).send({error:'User has no session!'});
+			}
+
 			/*** 
-			  bus boy is responsible for parsing multipart form data since express 4.0 droped multipart handling
-			  https://www.npmjs.com/package/busboy
+				bus boy is responsible for parsing multipart form data since express 4.0 droped multipart handling
+				https://www.npmjs.com/package/busboy
 			***/
 			var busboy = new Busboy({ headers: request.headers });
-		    // add listener when bus boy handles a file
-		    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-		      // create a unique id and append to filename so there isn't namesake clashing
-		      var uniqueId = uuid.v4();
-		      // asychronously write a file using a stream to uploads folder
-		      fstream = fs.createWriteStream(__dirname + '/uploads/' + uniqueId + filename);
-		      file.pipe(fstream);
+			// add listener when bus boy handles a file
+			busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+				// create a unique id and prepend to filename so there isn't namesake clashing
+				var uniqueId = uuid.v4();
+				// asychronously write a file using a stream to uploads folder
+				fstream = fs.createWriteStream(__dirname + '/uploads/' + uniqueId + filename);
+				file.pipe(fstream);
+				// add uniqueId and filename to user receiving download
+				users[userId].files.push(util.createFile(uniqueId, filename));
+				// emit a download prompt to the user that is receiving the upload
+				userSockets[userId].emit('requestDownload', filename);
+			});
 
-		      // add uniqueId and filename to user receiving download
-		      // users[req.user.id].files.push(util.createFile(uniqueId, filename));
+			// close request
+			busboy.on('finish', function() {
+				response.writeHead(303, { Connection: 'close', Location: '/' });
+				response.end();
+			});
 
-		      // emit a download prompt to the user that is receiving the upload
-		      // userSockets[1].emit('requestDownload', filename);
-		      
-		    });
-		   
-		    // close request
-		    busboy.on('finish', function() {
-		      response.writeHead(303, { Connection: 'close', Location: '/' });
-		      response.end();
-		    });
-		    request.pipe(busboy);
+			request.pipe(busboy);
 		},
 		download : function(request, response, error) {
-			// currently relies on front end to send uniqueId + filename
-			// Refactor: 
-			//	to use global user object
-			//	have error checking if file isn't found
-			//  user validation
+			// add middleware validation
+			var userId;
+			if(request.session.passport){
+				userId = request.session.passport.user
+			}else{
+				res.status(500).send({error:'User has no session!'});
+			}
+
 			var filepath = __dirname + '/uploads/' + request.query.uniqueId + request.query.filename;
 			var filename = path.basename(filepath);
 			var mimetype = mime.lookup(filepath);
@@ -98,15 +107,16 @@ module.exports = function(express, socketedServer){
 				response.send ({
 					status: "200",
 					response: {
-					filename: filename,
-					success: true
+						filename: filename,
+						success: true
 					}
 				});   
 			});
 		},
 		test: function(request, response, error) {
+			// add middleware validation
 			if(request.session.passport){
-				userId = request.session.passport.user
+				var userId = request.session.passport.user;
 				response.sendFile(path.resolve('controllers/test/test.html'));
 			}else{
 				response.send('login puhleaze');
